e40697ced7998cd1835f83843acb504b7c032023
Luis Mayta <luis.mayta@gmail.com>
2022-02-14 18:02:56 -0500

    Data santos

---
+++ android/app/src/main/kotlin/com/example/flutter_emanuel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_emanuel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/lista_santos.dart
@@ -0,0 +1,67 @@
+const List<Map<String, dynamic>> santos = [
+    {
+      'dia': 1,
+      'mes': 1,
+      'nombre': "SOLEMNIDAD DE LA VIRGEN MARÍA",
+      'imagen':'https://www.vaticannews.va/content/dam/vaticannews/santi/20171023_MV_Inv%2040170%20IFC_Capanna_MARIA%20SANTISSIMA%20MADRE%20DI%20DIO.jpg/_jcr_content/renditions/cq5dam.thumbnail.cropped.250.141.jpeg',
+      'descripcion': "El Concilio de Éfeso establece una verdad muy querida por el pueblo cristiano: María es verdadera Madre de Cristo, que es verdadero Dios y verdadero hombre: una sola Persona con dos naturalezas, sin confusión, sin mutación, sin división. En consecuencia es afirmada también su maternidad divina. ",
+      'enlace': "Nativo de Civitavecchia (1745-1824), contra la voluntad del padre se hace sacerdote pasionista. Gran predicador, obispo de Macerata: cuida la formación de los seminaristas y ayuda a los pobres. Después de negarse a jurar fidelidad a Napoleón, el Papa lo convoca a Roma como su consejero.",
+    },
+    {
+      'dia': 2,
+      'mes': 1,
+      'nombre': "SS. BASILIO EL GRANDE Y GREGORIO NACIANZENO, OBISPOS Y DOCTORES DE LA IGLESIA",
+      'imagen':'https://www.vaticannews.va/content/dam/vaticannews/santi/20171214_MV_Inv%2040089%20IFC_BASILIO%20MAGNO%20e%20GREGORIO%20NAZANZIENO.jpg/_jcr_content/renditions/cq5dam.thumbnail.cropped.250.141.jpeg',
+      'descripcion': "Dos Santos que en vida estuvieron unidos por una profunda amistad, y cuya memoria litúrgica se celebra el mismo día, el 2 de enero: San Basilio (a quien está dedicada la catedral de Moscú) y San Gregorio Nacianceno.  ",
+      'enlace':''
+    },
+    {
+      'dia': 3,
+      'mes': 1,
+      'nombre': 'S. ANTERO, PAPA',
+      'imagen':'https://www.vaticannews.va/content/dam/vaticannews/santi/20180103_Basilica%20di%20san%20Paolo%20fuori%20le%20mura_medaglioni%20papi_ANTERO.jpg/_jcr_content/renditions/cq5dam.thumbnail.cropped.250.141.jpeg',
+      'descripcion': 'De origen griego, Papa por solo 40 días. Es martirizado en 236 bajo el emperador Maximino el Tracio por haber recolectado las Actas de los mártires en los archivos de la Iglesia de Roma para que no se perdieran. Primer Papa sepultado en las catacumbas de Calixto.',
+      'enlace':''
+    },
+    {
+      'dia': 3,
+      'mes': 1,
+      'nombre': 'SANTA GENOVEVA DE PARÍS, VIRGEN',
+      'imagen':'',
+      'descripcion': 'Vivió alrededor de 500 en París, Genoveva, hija de nobles galo-romanos, eligió la vida ascética de las vírgenes consagradas en lugar del matrimonio. El carisma de sanaciòn y la santa vida de esta mujer mística le procuraron una gran fama de santidad. Hoy es la patrona de la capital francesa.',
+      'enlace':'https://www.vaticannews.va/es/santos/01/03/santa-genoveva-de-paris--virgen.html'
+    },
+    {
+      'dia': 3,
+      'mes': 1,
+      'nombre': 'S. JOSÉ TOMASI, SACERDOTE TEATINO, CARDENAL',
+      'imagen':'',
+      'descripcion': 'Nacido en Licata, en 1649, de la noble familia siciliana de Lampedusa, renuncia al título en favor del hermano y se hace religioso teatino. Precursor de la reforma litúrgica, es nombrado cardenal por el Papa Clemente XI en 1712. Juan Pablo II lo proclama Santo en 1986.',
+      'enlace':''
+    },
+    {
+      'dia': 3,
+      'mes': 1,
+      'nombre': 'S. TELÉSFORO, PAPA Y MARTIR',
+      'imagen':'',
+      'descripcion': 'Nacido en Calabria, de origen griego, anacoreta en Egipto y Palestina, Papa de 125 a 136. Instituyó la Misa de Medianoche en Navidad con el canto del “Gloria in excelsis Deo”, la celebración de la Pascua el domingo, el ayuno cuaresmal. Contrastó la gnosis. Murió mártir.  ',
+      'enlace':''
+    },
+    {
+      'dia': 3,
+      'mes': 1,
+      'nombre': '',
+      'imagen':'',
+      'descripcion': '',
+      'enlace':''
+    },
+    {
+      'dia': 4,
+      'mes': 1,
+      'nombre': '',
+      'imagen':'',
+      'descripcion': '',
+      'enlace':''
+    },
+
+  ];
